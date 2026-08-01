c109d0db8e60d3d9d330c01278d6cc99a3c9d29a
Varun Jatav <varunjatav.kv@gmail.com>
2026-08-01 19:32:21 +0530

    created separate test folder for testing files

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
-const app = require('./app');
+const app = require('../app');
 
 let mongoServer;
 
